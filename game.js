6831705f06397d1e17331bbcaa4ae1b9044d11e9
vector005awolesi <balogunaminat2017@gmail.com>
2025-10-04 15:56:39 +0100

    update

--- game.js
+++ game.js
@@ -1,26 +1,24 @@
-$("html").on("keypress", function (event) {
-  var startKey = event.key;
-  if (startKey === "a" || startKey === "A") {
-    gameInit()
-  } else {
-  }
-});
-function gameInit() {
-  var redBtn = $("div.btn.red");
-  var greenBtn = $("div.btn.green");
-  var yellowBtn = $("div.btn.yellow");
-  var blueBtn = $("div.btn.blue");
-  var allBtn = $("div[type='button']");
-  var gamePattern = [];
 
-  function nextSequence() {
-    var colors = ["red", "blue", "green", "yellow"];
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomColor = colors[randomNumber];
-    console.log(randomColor);
-    gamePattern.push(randomColor);
-    var currentBtn = $(`#${randomColor}`);
-    currentBtn.click(()=>{alert("clicked")})
-  }
-  nextSequence()
+var allBtn = $("div[type='button']");
+var gamePattern = [];
+var userClickedPattern = [];
+var colors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomColor = colors[randomNumber];
+
+  console.log(randomColor);
+  gamePattern.push(randomColor);
+  
+  var randomBtn = $(`#${randomColor}`);
+  randomBtn.fadeIn(100).fadeOut(100).fadeIn(100);
+  var sound = new Audio(`sounds/${randomColor}.mp3`);
+  sound.play();
+  allBtn.on("click", function () {
+    let id = $(this).attr("id");
+    userClickedPattern.push(id);
+    console.log(userClickedPattern);
+  });
 }
+nextSequence();
