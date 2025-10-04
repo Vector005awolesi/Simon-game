eb3e164786ff24f31dafb3725052634c30b46007
vector005awolesi <balogunaminat2017@gmail.com>
2025-10-04 15:10:03 +0100

    simon-game

--- game.js
+++ game.js
@@ -1,9 +1,26 @@
-var redBtn = $("div.btn.red");
-var greenBtn = $("div.btn.green");
-var yellowBtn = $("div.btn.yellow");
-var blueBtn = $("div.btn.blue");
-var allBtn = $("div[type='button']");
+$("html").on("keypress", function (event) {
+  var startKey = event.key;
+  if (startKey === "a" || startKey === "A") {
+    gameInit()
+  } else {
+  }
+});
+function gameInit() {
+  var redBtn = $("div.btn.red");
+  var greenBtn = $("div.btn.green");
+  var yellowBtn = $("div.btn.yellow");
+  var blueBtn = $("div.btn.blue");
+  var allBtn = $("div[type='button']");
+  var gamePattern = [];
 
-allBtn.click(function(){
-    alert("Button clicked!");
-})
+  function nextSequence() {
+    var colors = ["red", "blue", "green", "yellow"];
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomColor = colors[randomNumber];
+    console.log(randomColor);
+    gamePattern.push(randomColor);
+    var currentBtn = $(`#${randomColor}`);
+    currentBtn.click(()=>{alert("clicked")})
+  }
+  nextSequence()
+}
