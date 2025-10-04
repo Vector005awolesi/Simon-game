280c4c1d1a28878ba88cf443b223759b7c65e99e
vector005awolesi <balogunaminat2017@gmail.com>
2025-10-04 14:37:19 +0100

    start of simon game

---
+++ game.js
@@ -0,0 +1,9 @@
+var redBtn = $("div.btn.red");
+var greenBtn = $("div.btn.green");
+var yellowBtn = $("div.btn.yellow");
+var blueBtn = $("div.btn.blue");
+var allBtn = $("div[type='button']");
+
+allBtn.click(function(){
+    alert("Button clicked!");
+})
